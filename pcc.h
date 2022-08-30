abfe632a1f26b0b281d3322fd670fabcb7c99bb4
Puppas <1072022900@qq.com>
2022-08-30 08:31:36 +0800

    add header file

---
+++ pcc.h
@@ -0,0 +1,78 @@
+#ifndef PCC_H
+#define PCC_H
+
+#include <assert.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//
+// tokenize.c
+//
+
+typedef enum {
+    TK_PUNCT,
+    TK_NUM,
+    TK_EOF
+} TokenKind;
+
+struct Token {
+    TokenKind kind;
+    Token *next;
+    int val;
+    char *loc;
+    int len;
+};
+
+typedef struct Token Token;
+
+void error(char *fmt, ...);
+void error_at(char *loc, char *fmt, ...);
+void error_tok(Token *tok, char *fmt, ...);
+bool equal(Token *tok, char *op);
+Token *skip(Token *tok, char *op);
+Token *tokenize(char *input);
+
+
+//
+// parse.c
+//
+
+typedef enum {
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NEG,
+    ND_EQ,
+    ND_NE,
+    ND_LT,
+    ND_LE,
+    ND_NUM
+} NodeKind;
+
+
+struct Node {
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;
+};
+
+typedef struct Node Node;
+
+
+Node *parse(Token *tok);
+
+
+//
+// codegen.c
+//
+
+void codegen(Node *node);
+
+
+#endif /* PCC_H */
